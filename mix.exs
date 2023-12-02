f2a7c8d1665d905e9ef711d0a8ca8b218a908afa
Alexander Afanasyev <alexander.afanasyev@aviasales.ru>
2023-12-02 16:11:54 +0700

    Refactor resource composition facility to be backed by bindable of new major version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Resource.MixProject do
   use Mix.Project
 
   @name "Resource"
-  @version "0.1.0"
+  @version "1.0.0"
   @description "A common pattern to acquire a resource, perform some action on it and then run a finalizer, regardless of the outcome of the action"
   @repo_url "https://github.com/iamafanasyev/resource"
 
@@ -24,7 +24,7 @@ defmodule Resource.MixProject do
 
   defp deps() do
     [
-      {:bindable, "~> 0.1.0"},
+      {:bindable, "~> 1.0.0"},
       {:ex_doc, "~> 0.29", only: :dev, runtime: false}
     ]
   end
